949dabc2c9a5ac6ed23d89826cbbf88c6c01c753
AlexABA92 <alex1991020480@gmail.com>
2024-12-23 23:49:41 +0200

    #add lick to project page

--- src/app/projects/[uid]/page.tsx
+++ src/app/projects/[uid]/page.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from 'next'
+import next, { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { SliceZone } from '@prismicio/react'
 
@@ -7,6 +7,11 @@ import { components } from '@/slices'
 import Bounded from '@/app/components/Bounded'
 import Heading from '@/app/components/Heading'
 import { DateField, isFilled } from '@prismicio/client'
+import { IoLogoGithub } from 'react-icons/io'
+
+import { FaInternetExplorer } from 'react-icons/fa'
+import Link from 'next/link'
+import { PrismicNextLink } from '@prismicio/next'
 
 type Params = { uid: string }
 
@@ -39,6 +44,40 @@ export default async function Page({ params }: { params: Params }) {
 					{page.tags.map(tag => {
 						return <span key={tag}>#{tag}</span>
 					})}
+				</div>
+				<div>
+					{isFilled.link(page.data.githubprojectlink) && (
+						<div className='m-5 px-5 flex align-middle'>
+							<PrismicNextLink
+								field={page.data.githubprojectlink}
+								className='md:text-xl font-bold gap-2 ms-5 p-3 rounded-xl flex items-center bg-violet-700 hover:bg-indigo-800'
+								target='_blanck'
+							>
+								<IoLogoGithub
+									color='rgb(0, 255, 255)'
+									fontSize='2.25rem'
+									className='me-5'
+								/>
+								GitHyb Project linck
+							</PrismicNextLink>
+						</div>
+					)}
+					{isFilled.link(page.data.deploy_project_linck) && (
+						<div className='m-5 px-5 flex align-middle'>
+							<PrismicNextLink
+								field={page.data.deploy_project_linck}
+								className='md:text-xl font-bold gap-2 ms-5 p-3 rounded-xl flex items-center bg-violet-700 hover:bg-indigo-800'
+								target='_blanck'
+							>
+								<FaInternetExplorer
+									color='rgb(0, 255, 255)'
+									fontSize='2.25rem'
+									className='me-5'
+								/>
+								Link to Deploy
+							</PrismicNextLink>
+						</div>
+					)}
 				</div>
 				<p className='mt-8  border-b-2 border-slate-700 text-xl font-medium text-slate-300'>
 					{formatDate(page.data.date)}
